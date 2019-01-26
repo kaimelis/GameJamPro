d995937ebac3ca1fa574af889853b4289388c1b5
kaimelis <kaimelisvy24@outlook.com>
2019-01-26 14:41:47 +0100

    added layers and something boring

---
+++ GameJam/Assets/Scripts/Gameplay/LayerDatabse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GameJam
+{
+    public class LayerDatabse : MonoBehaviour
+    {   
+        [Serializable]
+        public struct ObjectInfo
+        {
+            public GameObject[] redObjects;
+            public GameObject[] blueObjects;
+            public GameObject[] greenObjects;
+        };
+
+        public ObjectInfo objectInfo;
+
+
+    }
+}
---
+++ GameJam/Assets/Scripts/Gameplay/LayerObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+    public class LayerObject : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            
+        }
+    }
+}
