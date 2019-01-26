77197652c01270b938808f2816abf2b7ec75604a
Mark <mhg2009@hotmail.com>
2019-01-26 13:20:11 +0100

    • Added fork throw • Added fork wave starts and ends

---
+++ GameJam/Assets/MarkTestShit/ForkTypeSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ForkType{Alpha, Beta, Gamma};
+
+public class ForkTypeSO : ScriptableObject
+{
+    //[SerializeField] private WaveEffect _effect;
+
+    [SerializeField] private ForkType _wavetype;
+    public float radius = 0;
+    public float weight = 0;
+
+    //The rate at which the radius deminishes in unit/s.
+    public float fadeRate = 0.5f;
+    public void EmitWave(){
+
+    }
+}
