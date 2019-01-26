374a012f72f93831abbc3511e0bcff79a5be3dba
kaimelis <kaimelisvy24@outlook.com>
2019-01-26 12:59:06 +0100

    deleted code for open srouce character controller.. too much. custom is better

---
+++ GameJam/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField]private float moveSpeed = 8.0f;
+        [SerializeField]private float jumpSpeed = 8.0f;
+        [SerializeField]private float gravity = 40.0f;
+        private float yVelocity = 0;
+        private bool isGrounded = false;
+        private CharacterController chController;
+        private Vector3 input;
+        private void Awake() 
+        {
+            chController = GetComponentInChildren<CharacterController>();
+        }
+
+        private void Update() 
+        {
+
+
+            if(chController.isGrounded)
+            {
+                yVelocity = 0;
+                input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+                if (Input.GetButtonDown("Jump"))
+                {
+                    yVelocity = jumpSpeed;
+                }
+            }
+            yVelocity -= gravity * Time.deltaTime;
+            chController.Move(transform.TransformDirection(input * moveSpeed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
+        }
+    }
+}
